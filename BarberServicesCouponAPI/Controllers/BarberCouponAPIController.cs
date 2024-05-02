9b9119aa2ad79e336cf7da122b0517f31086ea61
Nikhil Fenwick <78152038+nikhilfenwick@users.noreply.github.com>
2024-05-02 16:10:47 +0100

    Added CRUD today.
    Today I added delete, put, post and get.

--- BarberServicesCouponAPI/Controllers/BarberCouponAPIController.cs
+++ BarberServicesCouponAPI/Controllers/BarberCouponAPIController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BarberServicesCouponAPI.Data;
 using BarberServicesCouponAPI.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -38,6 +39,109 @@ namespace BarberServicesCouponAPI.Controllers
             var listOfAllBarberCoupons = _db.barberCoupons.ToList();
             return listOfAllBarberCoupons;
         } 
+
+        [HttpPost]
+        // Authorise will come here
+        [Route("AddingBarberCoupon")]
+        public BarberCoupon Post([FromBody] BarberCoupon barberCoupon)
+        {
+            try 
+            {
+                // Dto will come here
+                _db.barberCoupons.Add(barberCoupon);
+                _db.SaveChanges();
+
+
+            }
+
+            catch(Exception ex)
+            {
+                barberCoupon.BarberCouponCode = "Something went wrong";
+            }
+            return barberCoupon;
+        }
+
+        [HttpPut]
+        [Route("UpdatingBarberCoupon")]
+
+        public BarberCoupon Put([FromBody] BarberCoupon barberCoupon)
+        {
+            try 
+            {
+                // Mapper will come here
+                _db.barberCoupons.Update(barberCoupon);
+                _db.SaveChanges();
+
+                // Mapper will come here 
+
+            }
+
+            catch (Exception ex)
+            {
+                // _barberCoupon.IsSuccess = false; 
+                // _barberCoupon.Message = ex.Message;
+                // Dto will come here
+                barberCoupon.BarberCouponCode = "Something went wrong";
+
+            }
+
+            return barberCoupon;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public BarberCoupon Delete(int id)
+        {
+            BarberCoupon barberCoupon = new BarberCoupon();
+            try 
+            {
+                barberCoupon = _db.barberCoupons.First(u => u.BarberCouponId == id); 
+                _db.barberCoupons.Remove(barberCoupon);
+                _db.SaveChanges();  
+            }
+
+            catch (Exception ex)
+            {
+                // _barberCoupon.IsSuccess = false; 
+                // _barberCoupon.Message = ex.Message;
+                // Dto will come here
+                barberCoupon.BarberCouponCode = "Something went wrong";
+
+            }
+            
+            return barberCoupon;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public BarberCoupon GetByCode(int id)
+        {
+            BarberCoupon barberCoupon = new BarberCoupon(); 
+            try
+            {
+            
+            barberCoupon  = _db.barberCoupons.First(u => u.BarberCouponId == id);
+            //_response.Result = obj;
+            // _response.Result = _mapper.Map<CouponDTO>(obj); 
+            // Mapper will come here
+
+            }
+
+            catch (Exception e)
+            {
+                // _barberCoupon.IsSuccess = false; 
+                // _barberCoupon.Message = ex.Message;
+                // Dto will come here
+                barberCoupon.BarberCouponCode = "Something went wrong";
+
+            }
+
+            return barberCoupon;
+
+
+        }
+
 
     }
 
